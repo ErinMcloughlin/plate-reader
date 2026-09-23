05c002fdf731d2f29d7547e34196419fc95ad9ca
ErinMcloughlin <166519629+ErinMcloughlin@users.noreply.github.com>
2026-09-23 16:22:49 -0400

    app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ with st.container(border=True):
         expected_samples_count = st.number_input(
             "Expected Unique Samples:",
             min_value=1,
-            value=0,  
+            value=1,  
             step=1,
             help="The analytical pipeline will gatekeep processing until verified file rows match this value exactly."
         )
